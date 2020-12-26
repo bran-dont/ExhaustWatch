50cfb7b52500e01d83bc89d1214c4f898be9738d
bran-dont <hellobrandontran@gmail.com>
2020-12-26 15:00:12 -0800

    Initial Set-up
    * added activities and csvReader class

---
+++ app/src/main/java/com/example/exhaustwatch/CSVReader.kt
@@ -0,0 +1,26 @@
+package com.example.exhaustwatch
+
+import android.content.Context
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+
+class CSVReader(private var context: Context, private var fileName: String) {
+    var rows: MutableList<Array<String>> = ArrayList()
+
+    @Throws(IOException::class)
+    fun readCSV(): List<Array<String>> {
+        val `is`: InputStream = context.assets.open(fileName)
+        val isr = InputStreamReader(`is`)
+        val br = BufferedReader(isr)
+        var line: String
+        val csvSplitBy = ","
+        br.readLine()
+        while (br.readLine().also { line = it } != null) {
+            val row = line.split(csvSplitBy).toTypedArray()
+            rows.add(row)
+        }
+        return rows
+    }
+}
