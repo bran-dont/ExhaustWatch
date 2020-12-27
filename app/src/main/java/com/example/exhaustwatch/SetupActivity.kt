ede4c570d8ec17317be89d0fd4d04e2ef5850ac9
bran-dont <hellobrandontran@gmail.com>
2020-12-26 17:56:14 -0800

    added spinners to set-up activity and moved strings
    * added spinners to set-up activity for year, make, model
    * refactored strings in set-up activity to remove warnings

--- app/src/main/java/com/example/exhaustwatch/SetupActivity.kt
+++ app/src/main/java/com/example/exhaustwatch/SetupActivity.kt
@@ -1,17 +1,15 @@
 package com.example.exhaustwatch
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+
 
 class SetupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,71 +32,95 @@ class SetupActivity : AppCompatActivity() {
         }
 
 
-        mLoginBtn.setOnClickListener { startActivity(Intent(applicationContext, LoginActivity::class.java)) }
+        mLoginBtn.setOnClickListener { startActivity(
+            Intent(
+                applicationContext,
+                LoginActivity::class.java
+            )
+        ) }
 
 
-        mRegisterBtn.setOnClickListener(object: View.OnClickListener
-        {
+        mRegisterBtn.setOnClickListener(object : View.OnClickListener {
             @Override
-            override fun onClick(v: View)
-            {
+            override fun onClick(v: View) {
                 val email = mEmail.text.toString().trim()
                 val password = mPassword.text.toString().trim()
                 val fullName = mFullName.text.toString().trim()
                 val phoneNumber = mPhone.text.toString().trim()
 
-                if(TextUtils.isEmpty(fullName))
-                {
+                if (TextUtils.isEmpty(fullName)) {
                     mFullName.error = "Full Name is Required"
                     return
                 }
-                if(TextUtils.isEmpty(email))
-                {
+                if (TextUtils.isEmpty(email)) {
                     mEmail.error = "Email is Required"
                     return
                 }
-                if(TextUtils.isEmpty(password))
-                {
+                if (TextUtils.isEmpty(password)) {
                     mPassword.error = "Password is Required"
                     return
                 }
-                if(TextUtils.isEmpty(phoneNumber))
-                {
+                if (TextUtils.isEmpty(phoneNumber)) {
                     mPhone.error = "Phone is Required"
                     return
                 }
-                if(password.length < 6)
-                {
+                if (password.length < 6) {
                     mPassword.error = "Password must be at least 6 characters"
                     return
                 }
-                if(phoneNumber.length < 10)
-                {
+                if (phoneNumber.length < 10) {
                     mPhone.error = "Phone Number must be at least 10 characters"
                     return
                 }
                 //register user to firebase
 
-                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                    if(task.isSuccessful) {
-                        Toast.makeText(this@SetupActivity, "User Created.", Toast.LENGTH_SHORT).show()
-                        val userID = fAuth.currentUser?.uid
-                        val documentReference = userID?.let {
-                            fStore.collection("users").document(
+                fAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this@SetupActivity, "User Created.", Toast.LENGTH_SHORT)
+                                .show()
+                            val userID = fAuth.currentUser?.uid
+                            val documentReference = userID?.let {
+                                fStore.collection("users").document(
                                     it
-                            )
+                                )
+                            }
+                            val user = HashMap<String, String>()
+                            user["fName"] = fullName
+                            user["email"] = email
+                            user["phone"] = phoneNumber
+                            documentReference?.set(user)?.addOnSuccessListener {
+                                Log.d(
+                                    "TAG",
+                                    "on Success: user profile created for $userID"
+                                )
+                            }
+                            startActivity(Intent(applicationContext, MainActivity::class.java))
+                        } else {
+                            Toast.makeText(
+                                this@SetupActivity,
+                                "Error! " + task.exception.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
-                        val user = HashMap<String, String>()
-                        user["fName"] = fullName
-                        user["email"] = email
-                        user["phone"] = phoneNumber
-                        documentReference?.set(user)?.addOnSuccessListener { Log.d("TAG", "on Success: user profile created for $userID") }
-                        startActivity(Intent(applicationContext, MainActivity::class.java))
-                    } else {
-                        Toast.makeText(this@SetupActivity, "Error! " + task.exception.toString(), Toast.LENGTH_SHORT).show()
                     }
-                }
             }
         })
+        temporaryPopulateSpinners()
+    }
+
+    fun temporaryPopulateSpinners() {
+        val yearSpinner = findViewById<Spinner>(R.id.yearSpinner)
+        val makeSpinner = findViewById<Spinner>(R.id.makeSpinner)
+        val modelSpinner = findViewById<Spinner>(R.id.modelSpinner)
+
+        val years = ArrayList<Int>()
+        years.add(1994)
+        years.add(2000)
+
+        val arrayAdapter: ArrayAdapter<Int> = ArrayAdapter<Int>(this, android.R.layout.simple_spinner_item)
+        arrayAdapter.addAll(years)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        yearSpinner.adapter = arrayAdapter
     }
 }
