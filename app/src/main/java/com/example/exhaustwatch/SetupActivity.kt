1361567ae07fde758e2f1b765739bdeb2df294c4
bran-dont <hellobrandontran@gmail.com>
2020-12-26 16:26:27 -0800

    person class and spinner set-up
    * added person class to store user info locally
    * added spinners to be added to set-up xml

--- app/src/main/java/com/example/exhaustwatch/SetupActivity.kt
+++ app/src/main/java/com/example/exhaustwatch/SetupActivity.kt
@@ -2,6 +2,7 @@ package com.example.exhaustwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
 
 class SetupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
